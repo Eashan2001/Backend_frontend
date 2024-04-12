874b81c1f8699960af29b446a85e8f39d8e1f05a
eashan1809 <104688928+eashan1809@users.noreply.github.com>
2024-04-12 16:38:09 +0530

    push2

---
+++ src/models/video.model.js
@@ -0,0 +1,40 @@
+import mongoose ,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema=new Schema({
+    videoFile:{
+        type:String,
+        required:ture,
+    },
+    thumbnail:{
+        type:String,
+        required:ture,
+    },
+    title:{
+        type:String,
+        required:ture,
+    },
+    description:{
+        type:String,
+        required:ture,
+    },
+    duration:{
+        type:Number,
+        required:ture,
+    },
+    views:{
+        type:Number,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+},{timestamps:true})
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video=mongoose.model("Video",videoSchema)
